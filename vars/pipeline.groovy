38a5c4862b3a00b024c97cd2c72cbeb6ffdaf4c8
Austin Baugh <austin.baugh@smoothstack.com>
2022-01-28 22:40:29 -0800

    UTOPIA-79: Created shared jenkins library that will contain the sonarscan stage

---
+++ vars/pipeline.groovy
@@ -0,0 +1,94 @@
+import com.smoothstack.utopia.Git;
+
+def call() {
+    pipeline {
+        agent any
+
+        environment {
+            PROJECT_ID = "AB-utopia"
+
+            image = null
+            built = false
+        }
+
+        stages {
+            //TODO
+            //stage('SonarQube Analysis') {
+            //    steps {
+            //        withSonarQubeEnv('SonarQube') {
+            //            sh './mvnw org.sonarsource.scanner.maven:sonar-maven-plugin:3.7.0.1746:sonar'
+            //        }
+            //    }
+            //}
+
+            stage('Build') {
+                steps {
+                    sh "docker context use default"
+                    def image_label = "${PROJECT_ID.toLowerCase()}-$POM_ARTIFACTID"
+                    sh "docker tag $image_label -t ${getCommitSha().toSubList(0, 7)}"
+                    sh "docker tag $image_label -t $POM_VERSION"
+                    image = docker.build()
+                }
+            }
+
+            stage('Push to registry') {
+                steps {
+                    withCredentials([[
+                        $class: 'AmazonWebServicesCredentialsBinding',
+                        credentialsId: "jenkins",
+                        accessKeyVariable: 'AWS_ACCESS_KEY_ID',
+                        secretKeyVariable: 'AWS_SECRET_ACCESS_KEY'
+                    ]]) {
+                        script {
+                            def region = sh(
+                                script:'aws configure get region',
+                                returnStdout: true
+                            ).trim()
+                            def aws_account_id = sh(
+                                script:'aws sts get-caller-identity --query "Account" --output text',
+                                returnStdout: true
+                            ).trim()
+                            docker.withRegistry(
+                                ecr_uri = "${aws_account_id}.dkr.ecr.${region}.amazonaws.com"
+                                "https://$ecr_uri/$POM_ARTIFACTID",
+                                "ecr:$region:jenkins"
+                            ) {
+                                image.push('latest')
+                            }
+                        }
+                    }
+                }
+
+                post {
+                    cleanup {
+                        script {
+                            def image_label = "${PROJECT_ID.toLowerCase()}-$POM_ARTIFACTID"
+                            sh "docker rmi $image_label:${getCommitSha().toSubList(0, 7)}"
+                            sh "docker rmi $image_label:$POM_VERSION"
+                            sh "docker rmi $image_label:latest"
+                            sh "docker rmi $image_label"
+                        }
+                    }
+                }
+            }
+        }
+
+        post {
+            always {
+                script {
+                    if(built) {
+                        updateGithubCommitStatus({
+                            result: "SUCCESS",
+                            description: "Build complete"
+                        })
+                    } else {
+                        updateGithubCommitStatus({
+                            result: "FAILURE",
+                            description: "Build failed"
+                        })
+                    }
+                }
+            }
+        }
+    }
+}
