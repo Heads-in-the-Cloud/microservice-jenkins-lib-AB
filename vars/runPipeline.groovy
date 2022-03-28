ef17d18dd7f3c82e2a0df15f27fe325bed600387
Austin Baugh <austin@serene.cafe>
2022-03-27 22:56:08 -0700

    Fished commit hash typo

--- vars/runPipeline.groovy
+++ vars/runPipeline.groovy
@@ -120,7 +120,7 @@ def call() {
                     ]]) {
                         script {
                             sh "aws eks --region $region update-kubeconfig --name $PROJECT_ID"
-                            sh "kubectl -n microservices set image deployments/$POM_ARTIFACTID $POM_ARTIFACTID=$image_url:${getCommitHash().substring(0, 7)}"
+                            sh "kubectl -n microservices set image deployments/$POM_ARTIFACTID $POM_ARTIFACTID=$image_url:${getCommitSha().substring(0, 7)}"
                         }
                     }
                 }
