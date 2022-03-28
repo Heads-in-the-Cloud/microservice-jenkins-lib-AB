e4f0a31b4ea179d0ebc029f121105b9120ea59c6
Austin Baugh <austin@serene.cafe>
2022-03-27 23:37:32 -0700

    Change deployment tag to commit hash

--- vars/runPipeline.groovy
+++ vars/runPipeline.groovy
@@ -129,7 +129,7 @@ def call() {
                     ]]) {
                         script {
                             sh "aws eks --region $region update-kubeconfig --name $PROJECT_ID"
-                            sh "kubectl -n microservices set image deployments/$POM_ARTIFACTID $POM_ARTIFACTID=$image_url:latest"
+                            sh "kubectl -n microservices set image deployments/$POM_ARTIFACTID $POM_ARTIFACTID=$image_url:${getCommitSha().substring(0, 7)}"
                         }
                     }
                 }
