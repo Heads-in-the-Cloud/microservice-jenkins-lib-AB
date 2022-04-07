b7354066cec6b03ab2d9d2564ae81afdc337692f
Austin Baugh <austin@serene.cafe>
2022-04-07 11:36:00 -0700

    Switched to using environment variables when relevant

--- vars/runPipeline.groovy
+++ vars/runPipeline.groovy
@@ -1,8 +1,117 @@
-def getCommitSha() {
-  return sh(
-    script: "git rev-parse HEAD",
-    returnStdout: true
-  ).trim()
+def call() {
+    pipeline {
+        agent any
+
+        environment {
+            PROJECT_ID = 'AB-utopia'
+
+            def get_pom_value = { key -> sh(
+                script: "./mvnw help:evaluate -Dexpression=${key} -q -DforceStdout",
+                returnStdout: true
+            )}
+
+            POM_ARTIFACT_ID = get_pom_value('project.artifactId')
+            POM_VERSION = get_pom_value('project.version')
+            IMAGE_LABEL = "${PROJECT_ID.toLowerCase()}-$POM_ARTIFACT_ID"
+            GIT_COMMIT_HASH = sh(
+                script: 'git rev-parse HEAD',
+                returnStdout: true
+            ).trim()
+            EKS_CLUSTER_NAME = PROJECT_ID.toLowerCase()
+        }
+
+        stages {
+            stage('Package JAR with Maven') {
+                steps {
+                    sh './mvnw clean package'
+                }
+            }
+
+            stage('Code Quality Check') {
+                steps {
+                    withCredentials([
+                        string(credentialsId: "SonarQube Token", variable: 'SONAR_TOKEN')
+                    ]) {
+                        // SonarQube Analysis
+                        sh '''
+                            SonarQubeScanner-4.6.2/bin/sonar-scanner \
+                                -Dsonar.login=$SONAR_TOKEN \
+                                -Dsonar.projectKey=$PROJECT_ID-$POM_ARTIFACT_ID \
+                                -Dsonar.host.url=http://jenkins2.hitwc.link:9000 \
+                                -Dsonar.sources=./src/main/java/com/smoothstack/utopia \
+                                -Dsonar.java.binaries=./target/classes/com/smoothstack/utopia
+                        '''
+                    }
+                }
+            }
+
+            stage('Build Docker image') {
+                steps {
+                    script {
+                        sh 'docker context use default'
+                        image = docker.build(env.IMAGE_LABEL)
+                    }
+                }
+            }
+
+            stage('Push to registry') {
+                steps {
+                    withCredentials([[
+                        $class: 'AmazonWebServicesCredentialsBinding',
+                        credentialsId: 'jenkins',
+                        accessKeyVariable: 'AWS_ACCESS_KEY_ID',
+                        secretKeyVariable: 'AWS_SECRET_ACCESS_KEY'
+                    ]]) {
+                        script {
+                            def region = sh(
+                                script:'aws configure get region',
+                                returnStdout: true
+                            ).trim()
+                            def aws_account_id = sh(
+                                script:'aws sts get-caller-identity --query "Account" --output text',
+                                returnStdout: true
+                            ).trim()
+                            env.IMAGE_URL = "${aws_account_id}.dkr.ecr.${aws_region}.amazonaws.com/$IMAGE_LABEL"
+
+                            docker.withRegistry("https://${env.IMAGE_URL}", "ecr:$aws_region:jenkins") {
+                                image.push('latest')
+                                image.push(env.POM_VERSION)
+                                image.push(env.GIT_COMMIT_HASH)
+                            }
+                        }
+                    }
+                }
+
+                post {
+                    cleanup {
+                        script {
+                            sh 'docker rmi $IMAGE_LABEL'
+                            sh 'docker rmi $IMAGE_URL:latest'
+                            sh 'docker rmi $IMAGE_URL:$POM_VERSION'
+                            sh 'docker rmi $IMAGE_URL:$GIT_COMMIT_HASH'
+                        }
+                    }
+                }
+            }
+
+            stage("Deploy to EKS") {
+                steps {
+                    withCredentials([[
+                        $class: 'AmazonWebServicesCredentialsBinding',
+                        credentialsId: 'jenkins',
+                        accessKeyVariable: 'AWS_ACCESS_KEY_ID',
+                        secretKeyVariable: 'AWS_SECRET_ACCESS_KEY'
+                    ]]) {
+                        script {
+                            // get tf_info
+                            sh "aws eks --region ${tf_info.region} update-kubeconfig --name $EKS_CLUSTER_NAME"
+                            sh 'kubectl set image deployments/$POM_ARTIFACT_ID $POM_ARTIFACT_ID=$IMAGE_URL:$GIT_COMMIT_HASH'
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
 
 // TODO: additional testing needed
@@ -20,132 +129,4 @@ void setBuildStatus(String message, String state) {
       errorHandlers: [[$class: "ChangingBuildStatusErrorHandler", result: "UNSTABLE"]],
       statusResultSource: [ $class: "ConditionalStatusResultSource", results: [[$class: "AnyBuildResult", message: message, state: state]] ]
   ]);
-}
-
-def call() {
-    pipeline {
-        agent any
-
-        environment {
-            PROJECT_ID = "AB-utopia"
-
-            POM_ARTIFACTID = sh(
-                script: './mvnw help:evaluate -Dexpression=project.artifactId -q -DforceStdout',
-                returnStdout: true
-            )
-            POM_VERSION = sh(
-                script: './mvnw help:evaluate -Dexpression=project.version -q -DforceStdout',
-                returnStdout: true
-            )
-            SONARQUBE_ID = tool name: 'SonarQubeScanner-4.6.2'
-
-            image = null
-            built = false
-            image_label = "${PROJECT_ID.toLowerCase()}-$POM_ARTIFACTID"
-        }
-
-        stages {
-
-            stage('Maven Package') {
-                steps {
-                    sh "./mvnw clean package"
-                }
-            }
-
-            stage('SonarQube Analysis') {
-                steps {
-                    withCredentials([
-                        string(credentialsId: "SonarQube Token", variable: 'SONAR_TOKEN')
-                    ]) {
-                        sh """
-                            ${SONARQUBE_ID}/bin/sonar-scanner \
-                                -Dsonar.login=$SONAR_TOKEN \
-                                -Dsonar.projectKey=$PROJECT_ID-$POM_ARTIFACTID \
-                                -Dsonar.host.url=http://jenkins2.hitwc.link:9000 \
-                                -Dsonar.sources=./src/main/java/com/smoothstack/utopia \
-                                -Dsonar.java.binaries=./target/classes/com/smoothstack/utopia
-                        """
-                    }
-                }
-            }
-
-            stage('Build Docker image') {
-                steps {
-                    script {
-                        sh "docker context use default"
-                        image = docker.build(image_label)
-                    }
-                }
-            }
-
-            stage('Push to registry') {
-                steps {
-                    withCredentials([[
-                        $class: 'AmazonWebServicesCredentialsBinding',
-                        credentialsId: "jenkins",
-                        accessKeyVariable: 'AWS_ACCESS_KEY_ID',
-                        secretKeyVariable: 'AWS_SECRET_ACCESS_KEY'
-                    ]]) {
-                        script {
-                            region = sh(
-                                script:'aws configure get region',
-                                returnStdout: true
-                            ).trim()
-                            def aws_account_id = sh(
-                                script:'aws sts get-caller-identity --query "Account" --output text',
-                                returnStdout: true
-                            ).trim()
-                            def ecr_uri = "${aws_account_id}.dkr.ecr.${region}.amazonaws.com"
-                            image_url = "$ecr_uri/$image_label"
-
-                            docker.withRegistry("https://$image_url", "ecr:$region:jenkins") {
-                                image.push('latest')
-                                image.push(POM_VERSION)
-                                image.push(getCommitSha().substring(0, 7))
-                            }
-                        }
-                    }
-                }
-
-                post {
-                    cleanup {
-                        script {
-                            sh "docker rmi $image_label"
-                            sh "docker rmi $image_url:latest"
-                            sh "docker rmi $image_url:$POM_VERSION"
-                            sh "docker rmi $image_url:${getCommitSha().substring(0, 7)}"
-                        }
-                    }
-                }
-            }
-
-            stage("Deploy to EKS") {
-                steps {
-                    withCredentials([[
-                        $class: 'AmazonWebServicesCredentialsBinding',
-                        credentialsId: "jenkins",
-                        accessKeyVariable: 'AWS_ACCESS_KEY_ID',
-                        secretKeyVariable: 'AWS_SECRET_ACCESS_KEY'
-                    ]]) {
-                        script {
-                            sh "aws eks --region $region update-kubeconfig --name $PROJECT_ID"
-                            sh "kubectl -n microservices set image deployments/$POM_ARTIFACTID $POM_ARTIFACTID=$image_url:${getCommitSha().substring(0, 7)}"
-                        }
-                    }
-                }
-            }
-        }
-
-        post {
-            always {
-                script {
-                    if(built) {
-                        setBuildStatus("Build complete", "SUCCESS")
-                    } else {
-                        setBuildStatus("Build failed", "FAILURE")
-                    }
-                }
-            }
-        }
-    }
 }
